4c48c24f3349fef65d05b546aa56402c8e75b3a1
Juanje Ojeda <juanje.ojeda@gmail.com>
2020-04-09 19:51:02 +0100

    Minor change to make the test's name more natural

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.mark.parametrize("bin_file", [
     ('/usr/bin/git')
 ])
-def test_bin_file(host, bin_file):
+def test_binary_file(host, bin_file):
     """Validate that the basic binary files are insttalled"""
     binary = host.file(bin_file)
     assert binary.exists
