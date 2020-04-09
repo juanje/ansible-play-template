ea589b2410cadd15b13399bcc3df5f0b5f7a52e0
Juanje Ojeda <juanje.ojeda@gmail.com>
2020-04-09 14:31:22 +0100

    Basic ansible playbook with molecule config for testing

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+"""Role testing files using testinfra."""
+import pytest
+
+
+@pytest.mark.parametrize("bin_file", [
+    ('/usr/bin/git')
+])
+def test_bin_file(host, bin_file):
+    """Validate that the basic binary files are insttalled"""
+    binary = host.file(bin_file)
+    assert binary.exists
